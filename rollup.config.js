f760643fd4be7776fa9a1a85afb49dd6f24b7a1e
chengda300 <97393284+chengda300@users.noreply.github.com>
2025-04-10 02:09:12 +0800

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -7,15 +7,7 @@ import typescript from "@rollup/plugin-typescript";
 
 export default [{
     plugins: [modify({"@checkIsPluginClass": ""}), nodeResolve(), typescript()],
-    input: Object.fromEntries(
-        globSync("src/**/index.ts").map(file => [
-            path.relative(
-                "src",
-                file.slice(0, file.length - path.extname(file).length)
-            ),
-            file
-        ])
-    ),
+    input: "src/conductor/runner/index.ts",
     output: {
         plugins: [terser({
             module: true,
